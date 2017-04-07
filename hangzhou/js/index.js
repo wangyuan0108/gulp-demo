48a4dd573408d1be768938b63797b2b75964c5db
wangyuan0108 <18626869772@163.com>
2017-04-07 17:56:53 +0800

    gulp学习使用

---
+++ hangzhou/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by LF on 2017/4/7.
+ */
+$(function () {
+    slide();
+    left();
+});
+var slide=function (){
+    $(".slideBox").slide({
+        mainCell:".bd ul",
+        autoPlay:true
+    });
+};
+var left=function () {
+    $(".picScroll-left").slide({
+        titCell:".hd ul",
+        mainCell:".bd ul",
+        autoPage:true,
+        effect:"left",
+        // autoPlay:true,
+        vis:4,
+        trigger:"click"
+    });
+};
